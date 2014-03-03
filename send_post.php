22922d824d99bef182c5d7d5b982e6108ff0e540
jakep11 <j.pickett67@yahoo.com>
2014-03-02 22:09:34 -0800

    Update send_post.php

--- send_post.php
+++ send_post.php
@@ -4,4 +4,5 @@ $connect = pg_connect("host=localhost port=5432 dbname=jake password=Jake09!");
 //Sending form data to sql db.
 pg_query($connect,"INSERT INTO jake (one, two)
 VALUES (1, 2)";
+pg_close();
 ?>
